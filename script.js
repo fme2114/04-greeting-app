bcc0c1cb2c2c26a3f1d9efe86c8c4dda79a84366
Fetia Mai Eckert <fme2114@columbia.edu>
2025-06-23 09:12:15 +0000

    Implement welcome message functionality in script.js and update index.html structure

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-let userName = "Mai"
-console.log("Welcome, " + userName + "!") 
+let button = document.getElementById("welcomeBtn");
+let message = document.getElementById("welcomeMsg");
+let nameInput = document.getElementById("nameInput");
+
+button.onclick = function() {
+  let userName = nameInput.value;
+  if (userName === "") {
+    message.textContent = "Please enter your name!";
+  } else {
+    message.textContent = "Welcome, " + userName + "! You are awesome!";
+  }
+  nameInput.value = ""; // Clear the input field
+};
